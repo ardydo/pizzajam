90af279e9d4705ae3b07e078d60e3c32671e8f9d
André Andrade <ardy.aileron@gmail.com>
2018-02-11 02:21:18 -0200

    scene changing script change

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,7 @@ extends Control
 var savegame = file.new()
 var save_path = "user://savegame.save"
 
+# reload scene
 func reload():
 	get_tree().reload_current_scene()
 
@@ -11,11 +12,14 @@ func _ready():
 	pass
 
 func _input(event):
+	# quit
 	if event.is_action_pressed("ui_quit"):
 		get_tree().quit()
+	# restart
 	if event.is_action_pressed("ui_restart"):
 		reload()
 		
-func change(a):
+# changes scene faster and with debug
+func go(a):
 	print("changing screen to " + str(a))
 	get_tree().change_scene(a)
